95e2440c3677ff45284d662811db184669d6b467
Viljam Steinsett Strøm <steinsettVFX@gmail.com>
2024-05-28 21:40:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,45 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const hamburgernav = document.querySelector('#hamburger-nav');
+
+function showNav(){
+    hamburgernav.classList.add('show');
+}
+
+function hidewNav(){
+    hamburgernav.classList.remove('show');
+}
+var currPos = window.scrollY;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > currPos) {
+  //scroll up
+    hidewNav();
+  } else {
+  //scroll down
+    showNav();
+  }
+  currPos = window.scrollY;
+});
+
+const desktopnav = document.querySelector('#desktop-nav');
+
+function showNavd(){
+    desktopnav.classList.add('showd');
+}
+
+function hidewNavd(){
+    desktopnav.classList.remove('showd');
+}
+var currPos = window.scrollY;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > currPos) {
+  //scroll up
+    hidewNavd();
+  } else {
+  //scroll down
+    showNavd();
+  }
+  currPos = window.scrollY;
+});
